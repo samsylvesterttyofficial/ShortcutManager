fc944cab00e0cba1aae50e5836e16e1dec36199e
Silvester S <samsylvestertty@gmail.com>
2020-12-14 22:16:52 +0530

    1. Removed message box from utility function 2. Added message box in the caller

--- WindowsFormsApplication1/Program.cs
+++ WindowsFormsApplication1/Program.cs
@@ -14,13 +14,15 @@ namespace HotKeyMgr
         [STAThread]
         static void Main()
         {
-
+            
             if (!getPrevInstance())
             {
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
                 Application.Run(new MainForm());
             }
+            else
+                MessageBox.Show("Already Running", "Hot Key Mgr", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
 
@@ -36,18 +38,8 @@ namespace HotKeyMgr
             Process[] allProcessWithThisName
                          = Process.GetProcessesByName(currPrsName);
 
-            //if more than one process is running return true.
-            //which means already previous instance of the application 
-            //is running
-            if (allProcessWithThisName.Length > 1)
-            {
-                MessageBox.Show("Already Running", "Hot Key Mgr", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                return true; // Yes Previous Instance Exist
-            }
-            else
-            {
-                return false; //No Prev Instance Running
-            }
+            // Return true if the process is already runing. (More than 1)
+            return allProcessWithThisName.Length > 1;            
         }
 
     }
